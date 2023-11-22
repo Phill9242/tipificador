99791eefe9fc0d920fb5331aa0514e70783b1368
phill9242 <philcarvalho@live.com>
2023-11-22 10:36:27 -0300

    use classe to make the code more clear

--- tipificar_insumos.py
+++ tipificar_insumos.py
@@ -3,142 +3,135 @@ import json
 import os
 import sys
 from treinadores import scikit_learn
+from unidecode import unidecode
 
-class Argumentos:
+class Tipificador:
 
 	def __init__(self):
+		self.processar_argumentos()
+		self.verificar_arquivos()
+		self.instanciar_variaveis()
+		return
+	
+	def processar_argumentos(self):
 		parser = argparse.ArgumentParser(description='Processa os argumentos.')
 		parser.add_argument('-m', '--modelo', required=True, help='O nome do modelo a ser utilizado.')
-		parser.add_argument('-a', '--arquivo', required=False, help='O arquivo que contém os dados para tipificar.')
-		parser.add_argument('-l', '--log', required=False, help='Ative esta opção para gerar um log de precisão de todas as bases disponíveis')
+		parser.add_argument('-b', '--bases', required=False, help='As bases a serem tipificadas.\nSe o argumento não for utilizado todas as bases serão tipificadas')
 		args = parser.parse_args()
 
 		self.modelo = "_".join(sorted(args.modelo.split(" ")))
-		self.arquivo = args.arquivo if args.arquivo else None
-		self.log = args.log.split(" ") if args.log else None
+		self.bases = args.bases.split(" ") if args.bases else None
+		return
 
-		if self.arquivo:
-			self.checar_arquivo()
-
+	def verificar_arquivos(self):
+		if self.bases:
+			self.checar_arquivo_bases()
+		else:
+			bases_json = [arq for arq in os.listdir("./arquivos/bases") if os.path.isfile(os.path.join("./arquivos/bases", arq))]
+			self.bases = list(map(lambda base: base.split(".")[0], bases_json))
 		self.checar_modelo()
+		return
+
+	def instanciar_variaveis(self):
+		with open("arquivos/log.json", "r") as file:
+			self.log = json.load(file)
+		self.novo_log = {}
+		self.array_previsao = []
+		self.array_tipo_original = []
+		self.array_tipos = ["", "equipamento", "equipamento permanente", "mão de obra", "material", "serviços", "taxas", "outros", "", "administracao", "aluguel", "verba"]
+		return
 
 	def checar_modelo(self):
 		modelo_caminho = os.path.join("./modelos", self.modelo)
 		if not os.path.exists(modelo_caminho):
 			print(f"A pasta do modelo '{self.modelo}' não existe.")
 			exit()
-
 		if not os.path.exists(os.path.join(modelo_caminho, "vetorizador_tfidf.joblib")):
 			print("É necessário gerar o arquivo do Vetorizador")
 			exit()
-
-		if not os.path.exists(os.path.join(modelo_caminho, "unit_columns.joblib")):
+		if not os.path.exists(os.path.join(modelo_caminho, "unidades.joblib")):
 			print("É necessário gerar o arquivo de dummie das unidades")
 			exit()
-
 		if not os.path.exists(os.path.join(modelo_caminho, "modelo_treinado.joblib")):
 			print("É necessário gerar o arquivo do modelo")
 			exit()
+		return
 
-	def checar_arquivo(self):
-		caminho_arquivo = os.path.join("./arquivos/bases", f"{self.arquivo}.json")
-		if not os.path.exists(caminho_arquivo):
-			print(f"O arquivo '{self.arquivo}' não existe. Certifique-se de que ele está na pasta correta.")
-			exit()		
+	def checar_arquivo_bases(self):
+		for base in self.bases:
+			caminho_arquivo = os.path.join("./arquivos/bases", f"{base}.json")
+			if not os.path.exists(caminho_arquivo):
+				print(f"O arquivo '{base}' não existe. Certifique-se de que ele está na pasta correta.")
+				exit()
+		return	
 
-def comparador(base):
-	caminho_arquivo_previsao = f"./arquivos/previsoes/previsao_{base}.json"
-	caminho_arquivo_original = f"./arquivos/bases/{base}.json"
+	def gerar_log(self):
+		i = 0
+		self.novo_log = {}
+		for previsao in self.array_previsao:
+			original = self.array_tipo_original[i]
+			base = self.bases[i]
+			mensagens = self.log.get(self.modelo, {}).get(base, {}).get("erros", {})
+			self.percorrer_dicionario_previsoes(previsao, original, mensagens)
+			itens_totais = len(previsao)
+			erros = len(mensagens)
+			precisao = round(100 - (erros / itens_totais * 100), 2)
+			self.novo_log[base] = {}
+			self.novo_log[base]["erros"] = mensagens
+			self.novo_log[base]["precisao"] = str(precisao)
+			self.novo_log[base]["itens"] = str(itens_totais)
+			self.novo_log[base]["n_erros"] = str(erros)
+			i += 1
+		self.salvar_log()
+		return
+	
+	def salvar_log(self):
+			self.novo_log["precisao_geral"] = self.calcular_precisao_geral()
+			self.log[self.modelo] = self.novo_log
+			with open("arquivos/log.json", 'w') as file:
+				json.dump(self.log, file, indent=2)
+			print ("Arquivo de log gerado com sucesso em \"./arquivos/log.json\"")
+			return
 
-	with open(caminho_arquivo_previsao, 'r') as file_previsao, open(caminho_arquivo_original, 'r') as file_original:
-		previsao = json.load(file_previsao)
-		original = json.load(file_original)
-
-	array_tipos = ["", "equipamento", "permanente", "mão de obra", "material", "serviços", "taxas", "outros"]
-	erros = 0
-	mensagens = []
-
-	for codigo, valor in previsao.items():
-		if original[codigo]["tipo"] != valor["tipo"]:
-			descricao = original[codigo]['descricao']
-			tipo_preditor = array_tipos[valor['tipo']]
-			tipo_original = array_tipos[original[codigo]['tipo']]
-			mensagens.append(f"{codigo}: {descricao}\nPreditor: {tipo_preditor} | Original: {tipo_original}\n")
-			erros += 1
-
-	total_items = len(previsao)
-	precisao = 100 - (erros / total_items * 100)
-	print("\n".join(mensagens))
-	print(f"Erros = {erros}")
-	print(f"Precisão de {round(precisao, 2)}%")
-
-def gerar_log(args):
-	novo_log = {}
-	with open("arquivos/log.json", "r") as file:
-		log = json.load(file)
-
-	arquivos = [arq for arq in os.listdir("./arquivos/bases") if os.path.isfile(os.path.join("./arquivos/bases", arq))]
-
-	for arquivo in arquivos:
-		base = arquivo.split(".")[0]
-		caminho_base = os.path.join("./arquivos/bases/", arquivo)		
-		with open(caminho_base, 'r') as file:
-			original = json.load(file)
-
-		scikit_learn.preditor(args.modelo, base)
-		caminho_arquivo_previsao = f"./arquivos/previsoes/previsao_{base}.json"
-		caminho_arquivo_original = f"./arquivos/bases/{base}.json"
-
-		with open(caminho_arquivo_previsao, 'r') as arquivo_previsao, open(caminho_arquivo_original, 'r') as arquivo_original:
-			previsao = json.load(arquivo_previsao)
-			original = json.load(arquivo_original)
-
-		array_tipos = ["", "equipamento", "permanente", "mão de obra", "material", "serviços", "taxas", "outros"]
-		erros = 0
-		mensagens = []
-
+	def percorrer_dicionario_previsoes(self, previsao, original, mensagens):
 		for codigo, valor in previsao.items():
 			if original[codigo]["tipo"] != valor["tipo"]:
-				descricao = original[codigo]['descricao']
-				tipo_preditor = valor['tipo']
-				tipo_original = original[codigo]['tipo'] 
-				if valor['tipo'] <= 7:
-					tipo_preditor = array_tipos[valor['tipo']]
-				if original[codigo]['tipo'] <= 7:
-					tipo_original = array_tipos[original[codigo]['tipo']]
-				mensagens.append({"codigo": f"{codigo}", "descricao": f"{descricao}", "preditor": f"{tipo_preditor}", "original": f"{tipo_original}"})
-				erros += 1
+				descricao = self.transliteracao(original[codigo]['descricao'])
+				tipo_preditor = unidecode(self.array_tipos[valor['tipo']])
+				tipo_original = unidecode(self.array_tipos[original[codigo]['tipo']])
+				mensagens[codigo] = {"codigo": f"{codigo}", "descricao": f"{descricao}", "preditor": f"{tipo_preditor}", "original": f"{tipo_original}"}
+		return 
 
-		itens_totais = len(previsao)
-		precisao = round(100 - (erros / itens_totais * 100), 2)
-		novo_log[base] = {}
-		novo_log[base]["erros"] = mensagens
-		novo_log[base]["precisao"] = str(precisao)
-		novo_log[base]["itens"] = str(itens_totais)
-		novo_log[base]["n_erros"] = str(erros)
+	def transliteracao(self, descricao):
+		return unidecode(descricao).upper()
 
-	novo_log["precisao_geral"] = calcular_precisao_geral(novo_log)
-
-	log[args.modelo] = novo_log
-	with open("arquivos/log.json", 'w') as file:
-		json.dump(log, file, indent=2)
-
-def calcular_precisao_geral(hash):
-	erros = 0
-	itens = 0
-	for _, sub_hash in hash.items():
-		erros += int(sub_hash["n_erros"])
-		itens += int(sub_hash["itens"])
-	return str(round(100 - (erros / itens * 100), 2))
+	def preditor(self):
+		for base in self.bases:
+			scikit_learn.preditor(self.modelo, base)
+			self.abrir_arquivos_json(base)
+		return
+	
+	def abrir_arquivos_json(self, base):
+		caminho_arquivo_previsao = f"./arquivos/previsoes/previsao_{base}.json"
+		caminho_arquivo_original = f"./arquivos/bases/{base}.json"
+		with open(caminho_arquivo_previsao, 'r') as arquivo_previsao, open(caminho_arquivo_original, 'r') as arquivo_original:
+			self.array_previsao.append(json.load(arquivo_previsao))
+			self.array_tipo_original.append(json.load(arquivo_original))
+		return
+	
+	def calcular_precisao_geral(self):
+		erros = 0
+		itens = 0
+		for _, sub_hash in self.novo_log.items():
+			erros += int(sub_hash["n_erros"])
+			itens += int(sub_hash["itens"])
+		return str(round(100 - (erros / itens * 100), 2))
+# FIM CLASSE TIPIFICADOR
 
 def main():
-	args = Argumentos()
-	if args.arquivo:
-		scikit_learn.preditor(args.modelo, args.arquivo)
-		comparador(args.arquivo)
-		
-	if args.log:
-		gerar_log(args)
+	tipificador = Tipificador()
+	tipificador.preditor()
+	tipificador.gerar_log()
 
 if __name__ == "__main__":
 	main()
--- treinadores/scikit_learn.py
+++ treinadores/scikit_learn.py
@@ -1,4 +1,5 @@
 import json
+from random import randint
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
@@ -12,31 +13,33 @@ from unidecode import unidecode
 ARQUIVO_TREINO = './arquivos/treinador.json'
 
 def treinador(nome_modelo):
-
+    numero_aleatorio = randint(1, 100)
+    print (numero_aleatorio)
+    tamanho_teste = 0.01
     with open(ARQUIVO_TREINO, 'r') as file:
         data = json.load(file)
 
-    descriptions = [item['descricao'] for item in data.values()]
+    descricoes = [item['descricao'] for item in data.values()]
     units = [item['unidade'] for item in data.values()]
-    types = [item['tipo'] for item in data.values()]
+    tipos = [item['tipo'] for item in data.values()]
     preco = [item['precos_iguais'] for item in data.values()]
   
     tfidf = TfidfVectorizer()
-    X_desc = tfidf.fit_transform(descriptions)
+    descricoes_tfidf = tfidf.fit_transform(descricoes)
 
-    df_units = pd.get_dummies(units)
+    unidades_dummie = pd.get_dummies(units)
     preco_array = np.array(preco).reshape(-1, 1)
 
-    X = np.hstack((X_desc.toarray(), df_units, preco_array))
-    y = np.array(types)
+    x_dados = np.hstack((descricoes_tfidf.toarray(), unidades_dummie, preco_array))
+    array_tipos = np.array(tipos)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=13)
+    x_dados_treino, x_dados_teste, array_tipos_treino, array_tipos_teste = train_test_split(x_dados, array_tipos, test_size=tamanho_teste, random_state=numero_aleatorio)
 
-    classifier = RandomForestClassifier(random_state=42)
-    classifier.fit(X_train, y_train)
+    classifier = RandomForestClassifier(random_state=numero_aleatorio)
+    classifier.fit(x_dados_treino, array_tipos_treino)
 
-    y_pred = classifier.predict(X_test)
-    print(classification_report(y_test, y_pred))
+    y_pred = classifier.predict(x_dados_teste)
+    print(classification_report(array_tipos_teste, y_pred))
 
     diretorio_base_modelos = './modelos'
     diretorio_modelo = os.path.join(diretorio_base_modelos, nome_modelo)
@@ -45,36 +48,36 @@ def treinador(nome_modelo):
 
     dump(classifier, os.path.join(diretorio_modelo, 'modelo_treinado.joblib'))
     dump(tfidf, os.path.join(diretorio_modelo, 'vetorizador_tfidf.joblib'))
-    dump(df_units.columns.tolist(), os.path.join(diretorio_modelo, 'unit_columns.joblib'))
+    dump(unidades_dummie.columns.tolist(), os.path.join(diretorio_modelo, 'unidades.joblib'))
     print(f'Novo modelo treinado: {nome_modelo}')
 
 
 def preditor(pasta_modelo, base):
-    classifier = load(f'./modelos/{pasta_modelo}/modelo_treinado.joblib')
+    modelo = load(f'./modelos/{pasta_modelo}/modelo_treinado.joblib')
     tfidf = load(f'./modelos/{pasta_modelo}/vetorizador_tfidf.joblib')
-    unit_columns = load(f'./modelos/{pasta_modelo}/unit_columns.joblib')
+    unidades_treino = load(f'./modelos/{pasta_modelo}/unidades.joblib')
 
     caminho_arquivo = os.path.join('./arquivos/bases', f"{base}.json" )
     with open(caminho_arquivo, "r") as file:
-        dados_sem_tipo = json.load(file)
+        arquivo_base = json.load(file)
 
-    new_descriptions = [transliteracao(item['descricao']) for item in dados_sem_tipo.values()]
-    new_units = [item['unidade'] for item in dados_sem_tipo.values()]
-    new_preco = [item['precos_iguais'] for item in dados_sem_tipo.values()]
+    descricoes = [transliteracao(item['descricao']) for item in arquivo_base.values()]
+    unidades = [item['unidade'] for item in arquivo_base.values()]
+    preco = [item['precos_iguais'] for item in arquivo_base.values()]
 
-    X_new_desc = tfidf.transform(new_descriptions)
+    descricoes_tfidf = tfidf.transform(descricoes)
 
-    new_df_units = pd.get_dummies(new_units).reindex(columns=unit_columns, fill_value=0)
-    new_preco_array = np.array(new_preco).reshape(-1, 1)
+    unidades_dummie = pd.get_dummies(unidades).reindex(columns=unidades_treino, fill_value=0)
+    preco_array = np.array(preco).reshape(-1, 1)
 
-    X_new = np.hstack((X_new_desc.toarray(), new_df_units, new_preco_array))
-    predicted_types = classifier.predict(X_new)
+    x_dados = np.hstack((descricoes_tfidf.toarray(), unidades_dummie, preco_array))
+    previsoes = modelo.predict(x_dados)
 
-    for i, item_id in enumerate(dados_sem_tipo):
-        dados_sem_tipo[item_id]['tipo'] = int(predicted_types[i])
+    for i, item_id in enumerate(arquivo_base):
+        arquivo_base[item_id]['tipo'] = int(previsoes[i])
 
     with open(f'./arquivos/previsoes/previsao_{base}.json', 'w') as file:
-        json.dump(dados_sem_tipo, file, indent=2)
+        json.dump(arquivo_base, file, indent=2)
 
     print(f'Arquivo salvo com sucesso: previsao_{base}.json')
 
